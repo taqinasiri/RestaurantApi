593d42216eb08b3873826b7896a4a00cc9fed1c4
Taqi ツ <mtaqinasiria@gmail.com>
2024-01-23 22:19:09 +0330

    Add AuditalbeShowProps To EntityBase

--- src/Core/Restaurant.Domain/Common/EntityBase.cs
+++ src/Core/Restaurant.Domain/Common/EntityBase.cs
@@ -1,6 +1,6 @@
 ﻿namespace Restaurant.Domain.Common;
 
-public abstract class EntityBase
+public abstract class EntityBase : IAuditableEntity
 {
     public long Id { get; set; }
     public bool IsDeleted { get; set; }
---
+++ src/Infrastructure/Restaurant.Persistence/Migrations/20240123184757_addAuditableShaowProps.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Restaurant.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class addAuditableShaowProps : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedByBrowserName",
+                table: "Categories",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedByIp",
+                table: "Categories",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "CreatedByUserId",
+                table: "Categories",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDateTime",
+                table: "Categories",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedByBrowserName",
+                table: "Categories",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedByIp",
+                table: "Categories",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "ModifiedByUserId",
+                table: "Categories",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDateTime",
+                table: "Categories",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedByBrowserName",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedByIp",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedByUserId",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDateTime",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedByBrowserName",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedByIp",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedByUserId",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDateTime",
+                table: "Categories");
+        }
+    }
+}
