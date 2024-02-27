a88d212dbb00c3d2908cbe3fa4529773b4fad342
Taqi ツ <mtaqinasiria@gmail.com>
2024-02-27 20:46:01 +0330

    Add | Sms Sender Service (Kavenegar)

---
+++ src/Infrastructure/Restaurant.Infrastructure/Sms/KavenegarSmsSenderService.cs
@@ -0,0 +1,38 @@
+﻿using Kavenegar;
+using Microsoft.Extensions.Options;
+using Restaurant.Application.Models;
+
+namespace Restaurant.Infrastructure.Sms;
+
+public class KavenegarSmsSenderService(IOptionsMonitor<SiteSettings> options) : ISmsSenderService
+{
+    private readonly SmsSettings _smsConfig = options.CurrentValue.SmsSettings;
+
+    public async ValueTask<bool> SendOTP(string receptor,string templateName,string token1,string? token2 = "",string? token3 = "")
+    {
+        try
+        {
+            var api = new KavenegarApi(_smsConfig.ApiKey);
+            await api.VerifyLookup(receptor,templateName,token1,token2,token3);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public async ValueTask<bool> SendPublic(string receptor,string message)
+    {
+        try
+        {
+            var api = new KavenegarApi(_smsConfig.ApiKey);
+            await api.Send(_smsConfig.Sender,receptor,message);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
